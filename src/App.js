bc18ed1f5464a0bf09f2ae2fb4e634499a879c18
julenbero <julenbero@gmail.com>
2020-10-26 20:53:15 -0500

    Update Increment Counter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			counter : 0
+		};
+	}
+
+	IncrementCounter = () => {
+		this.setState({ counter: this.state.counter + 1 });
+	};
+
+	render() {
+		return (
+			<div>
+				<span className="value">{this.state.counter}</span>
+				<button onClick={this.IncrementCounter} id="inc">
+					Incrementa
+				</button>
+			</div>
+		);
+	}
 }
 
 export default App;
